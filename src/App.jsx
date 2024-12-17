a20dade4ae40f56f306f6384cf58e7db0dda5c3f
Michael Nunez <mnunez1013@gmail.com>
2024-12-17 16:59:44 -0600

    added basic login from supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState,  useEffect } from "react";
 import styles from './App.module.css';
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { createClient } from '@supabase/supabase-js'
 
 const App = () => {
   const [text, setText] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [session, setSession] = useState();
+  const supabase = createClient('https://cymccahfqxuoirpkxlkh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5bWNjYWhmcXh1b2lycGt4bGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzODE4MDMsImV4cCI6MjA0OTk1NzgwM30.RG3mYI5xOVrktwft3-76Gwh88ak0iY-SMw8JMq1UH90')
+
+  useEffect(() => {
+      supabase.auth.getSession().then(({ data: { session } }) => {
+        setSession(session)
+      })
+      const {
+        data: { subscription },
+      } = supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session)
+      })
+      return () => subscription.unsubscribe()
+    }, [])
 
   // pull in data to display, below is just fake data for now
   // cRud = > Read
@@ -50,7 +66,7 @@ const App = () => {
 
   return (
     <div>
-      {isLoggedIn ? (
+      {session ? (
         <div className={styles.container}>
           Simple To Do List
           <ul style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
@@ -72,9 +88,7 @@ const App = () => {
           <button onClick={handleUpdate}>Save</button>
         </div>
       ) : (
-        <>
-          LOG IN STUFF
-        </>
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
       )}
     </div>
   );
