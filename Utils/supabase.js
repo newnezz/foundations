7b94008af660e9ba734ac923828c606150316c1b
Michael Nunez <mnunez1013@gmail.com>
2024-12-19 14:21:43 -0600

    mostly done, just a few more db issues to fix

--- Utils/supabase.js
+++ Utils/supabase.js
@@ -1,30 +1,7 @@
-import "./index.css";
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://cymccahfqxuoirpkxlkh.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5bWNjYWhmcXh1b2lycGt4bGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzODE4MDMsImV4cCI6MjA0OTk1NzgwM30.RG3mYI5xOVrktwft3-76Gwh88ak0iY-SMw8JMq1UH90";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-// import { useEffect, useState } from 'react'
-
-// import { Auth } from '@supabase/auth-ui-react'
-// import { ThemeSupa } from '@supabase/auth-ui-shared'
-// import { createClient } from '@supabase/supabase-js'
-
-// const supabase = createClient('https://cymccahfqxuoirpkxlkh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5bWNjYWhmcXh1b2lycGt4bGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzODE4MDMsImV4cCI6MjA0OTk1NzgwM30.RG3mYI5xOVrktwft3-76Gwh88ak0iY-SMw8JMq1UH90')
-
-export default function App() {
-  // const [session, setSession] = useState(null)
-  // useEffect(() => {
-  //   supabase.auth.getSession().then(({ data: { session } }) => {
-  //     setSession(session)
-  //   })
-  //   const {
-  //     data: { subscription },
-  //   } = supabase.auth.onAuthStateChange((_event, session) => {
-  //     setSession(session)
-  //   })
-  //   return () => subscription.unsubscribe()
-  // }, [])
-  // if (!session) {
-  //   return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
-  // }
-  // else {
-  //   return (<div>Logged in! Access Token: {session.access_token}</div>)
-  // }
-}
+export default supabase;
--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,168 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import styles from "./App.module.css";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { createClient } from "@supabase/supabase-js";
+import supabase from "../Utils/supabase";
 
 const App = () => {
   const [text, setText] = useState("");
-  const [session, setSession] = useState();
-  const supabase = createClient(
-    "https://cymccahfqxuoirpkxlkh.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5bWNjYWhmcXh1b2lycGt4bGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzODE4MDMsImV4cCI6MjA0OTk1NzgwM30.RG3mYI5xOVrktwft3-76Gwh88ak0iY-SMw8JMq1UH90",
-  );
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [todolist, setTodoList] = useState([]);
+  let user = undefined;
+
+  useEffect(() => {});
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-    return () => subscription.unsubscribe();
-  }, []);
+    checkSession();
 
-  const [fakeData, setFakeData] = useState([
-    {
-      id: 1,
-      title: "Sample 1",
-      completed: true,
-    },
-    {
-      id: 2,
-      title: "Sample 2",
-      completed: false,
-    },
-    {
-      id: 3,
-      title: "Sample 3",
-      completed: true,
-    },
-  ]);
+    console.log("user", user);
 
-  const inputGood = () => {
-    if (text.trim() === "") {
-      return "Cannot be empty";
+    const fetchTodos = async () => {
+      const { data, error } = await supabase.from("todos").select();
+
+      if (error) {
+        console.error(error);
+        setTodoList([]);
+        return;
+      }
+
+      if (data) {
+        setTodoList(data);
+        console.log("fetched list", data);
+      }
+    };
+
+    loggedIn && fetchTodos();
+  }, [loggedIn]);
+
+  const checkSession = async () => {
+    const { data: session, error } = await supabase.auth.getSession();
+    if (error) {
+      console.error("Error fetching session:", error);
+      return;
     }
-    if (fakeData.find((item) => item.title === text.trim())) {
-      return "Duplicate Entry";
-    }
-    return "good";
-  };
 
-  const handleAdd = () => {
-    if (inputGood() == "good") {
-      setFakeData([
-        ...fakeData,
-        { id: fakeData.length + 1, title: text, completed: false },
-      ]);
-      setText("");
+    if (session.session) {
+      console.log("Session exists:", session);
+      // Set session to true or store the user's info in state
+      setLoggedIn(true);
+      user = session.user; // Optional: Save user details
     } else {
-      alert(inputGood());
+      console.log("No active session");
+      setLoggedIn(false);
     }
   };
 
-  const handleDelete = (id) => {
-    setFakeData(fakeData.filter((item) => item.id !== id));
+  const handleAdd = useCallback(async () => {
+    console.log("handleadd user", user);
+    const { data, error } = await supabase
+      .from("todos")
+      .insert([
+        {
+          completed: false,
+          title: text,
+          user_id: user.id,
+        },
+      ])
+      .select();
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    if (data) {
+      console.log(data);
+      setTodoList([...todolist, { id: todolist.length + 1, title: text }]); // Crud > create
+      setText("");
+    }
+  });
+
+  const handleUpdate = async () => {
+    // const { data, error } = await supabase
+  };
+
+  const handleDelete = async (id) => {
+    const { error } = await supabase.from("todos").delete().eq("id", id);
+
+    if (!error) {
+      setTodoList(todolist.filter((item) => item.id !== id));
+    } else {
+      console.error(error);
+    }
   };
 
   const onCompleted = (item) => {
-    setFakeData(
-      fakeData.map((dataItem) =>
+    setTodoList(
+      todolist.map((dataItem) =>
         dataItem.id === item.id
-          ? { ...dataItem, completed: !dataItem.completed }
+          ? { ...dataItem, completed: !dataItem.completed } // Toggle completed status
           : dataItem,
       ),
     );
   };
 
+  const handleSignUp = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error);
+      setSession(false);
+      return;
+    }
+    if (data) {
+      setSession(true);
+      user = data.session.user;
+      console.log(data);
+    }
+  };
+
+  const handleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error);
+      setLoggedIn(false);
+      return;
+    }
+    if (data) {
+      setLoggedIn(true);
+      user = data.session.user;
+      console.log("handle login data", data);
+    }
+  };
+
+  const handleLogOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    setLoggedIn(false);
+    if (error) console.error(error);
+  };
+
   return (
     <div className={styles.container}>
-      {session ? (
+      {loggedIn ? (
         <div>
           Simple To Do List
           <ul style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-            {fakeData.map((item) => {
+            {todolist.map((item, key) => {
               return (
-                <div key={item.id} style={{ display: "flex", gap: "10px" }}>
+                <div style={{ display: "flex", gap: "10px" }}>
                   <li
+                    key={item.id}
                     onClick={() => onCompleted(item)}
-                    style={{
-                      textDecoration: item.completed ? "line-through" : "none",
-                      cursor: "pointer", // Indicates the item is clickable
-                    }}
+                    style={{ textDecoration: item.completed && "line-through" }}
                   >
                     {item.title}
                   </li>
                   <i
-                    className={`fa ${item.completed ? "fa-check-circle" : "fa-circle"}`}
-                    onClick={() => onCompleted(item)}
-                    style={{
-                      cursor: "pointer", // Add pointer cursor for the icon
-                      color: item.completed ? "green" : "gray",
-                    }}
-                  />
-                  <i
-                    className="fa fa-trash"
+                    class="fa fa-trash"
                     onClick={() => handleDelete(item.id)}
-                    style={{ cursor: "pointer", color: "red" }}
                   />
                 </div>
               );
@@ -119,14 +174,38 @@ const App = () => {
               type="text"
               onChange={(e) => setText(e.target.value)}
               value={text}
-            />
-            <div className={styles.button}>
+            ></input>
+            <div className="buttons">
               <button onClick={handleAdd}>Add New</button>
+              <button onClick={handleLogOut}>Log Out</button>
             </div>
           </div>
         </div>
       ) : (
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+        <div
+          style={{
+            display: "flex",
+            gap: "25px",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <h4 style={{ textAlign: "center", margin: 0 }}>Sign Up / Login</h4>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <button onClick={handleSignUp}>Sign Up</button>
+            <button onClick={handleLogin}>Login</button>
+          </div>
+        </div>
       )}
     </div>
   );
